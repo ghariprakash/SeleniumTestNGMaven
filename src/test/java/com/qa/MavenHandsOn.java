67c9cb2627ee2db265938e016cf090c1b43134a7
unknown <ghariprakash@gmail.com>
2018-08-12 11:47:45 +0530

    File MavenHandsOn.java has been changed

--- src/test/java/com/qa/MavenHandsOn.java
+++ src/test/java/com/qa/MavenHandsOn.java
@@ -10,7 +10,8 @@ public class MavenHandsOn {
 @Test
 void launchBrowser() {
 	
-	// Using  Using FirefoxOptions:		
+	// Using  Using FirefoxOptions:	
+	// Working Fine
 	System.setProperty("webdriver.gecko.driver", "E:\\Work\\Selenium\\JARs\\Browser_Drivers\\Gecko\\v0.19.0\\geckodriver-v0.19.0-win64\\geckodriver.exe");
 	FirefoxOptions options = new FirefoxOptions();
 	WebDriver driver= new FirefoxDriver(options); 
