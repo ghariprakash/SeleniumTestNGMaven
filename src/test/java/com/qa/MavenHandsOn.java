6081d62f948472a6790598bdb85fca7d9c2f8dec
unknown <ghariprakash@gmail.com>
2018-08-12 08:08:02 +0530

    First Commit for SeleniumTestNGMaven

---
+++ src/test/java/com/qa/MavenHandsOn.java
@@ -0,0 +1,21 @@
+package com.qa;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
+
+public class MavenHandsOn {
+	
+@Test
+void launchBrowser() {
+	
+	// Using  Using FirefoxOptions:		
+	System.setProperty("webdriver.gecko.driver", "E:\\Work\\Selenium\\JARs\\Browser_Drivers\\Gecko\\v0.19.0\\geckodriver-v0.19.0-win64\\geckodriver.exe");
+	FirefoxOptions options = new FirefoxOptions();
+	WebDriver driver= new FirefoxDriver(options); 
+	driver.get("http://www.google.com");	
+	driver.quit();
+}
+
+}
